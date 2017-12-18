53f964107b44057977aa5915fa6f06224b874874
Trey Duskin <trey@swiftstack.com>
2017-12-17 19:28:30 -0500

    bunch of stuff

--- tweepy-weepy.py
+++ tweepy-weepy.py
@@ -9,6 +9,10 @@ CONSUMER_SECRET = os.getenv('TWEEPY_CONSUMER_SECRET')
 ACCESS_CREDS_FILE = 'access.creds'
 
 
+auth = tweepy.OAuthHandler(consumer_key=CONSUMER_TOKEN,
+                           consumer_secret=CONSUMER_SECRET)
+
+
 def get_access_creds():
     try:
         redirect_url = auth.get_authorization_url()
@@ -31,37 +35,82 @@ def get_access_creds():
                      'access_token_secret': auth.access_token_secret}
         json.dump(credsdict, credsfile)
 
-auth = tweepy.OAuthHandler(consumer_key=CONSUMER_TOKEN,
-                           consumer_secret=CONSUMER_SECRET)
 
-if os.path.exists(ACCESS_CREDS_FILE):
-    print 'found %s! attempting to load access creds' % ACCESS_CREDS_FILE
-    try:
-        credsfile = open(ACCESS_CREDS_FILE)
-        credsdict = json.load(credsfile)
-        auth.set_access_token(credsdict['access_token'],
-                              credsdict['access_token_secret'])
-    except ValueError:
-        print 'problem with creds file! requesting new creds'
+def exit_menu(api):
+    return False
+
+
+def my_stats(api):
+    apiuser = api.me()
+    print "User %s (%s)" % (apiuser.name, apiuser.screen_name)
+    print "Tweets: %d" % apiuser.statuses_count
+    print "Followers: %d" % apiuser.followers_count
+    return True
+
+
+def list_tweets(api):
+    return
+
+
+def wipe_timeline(api):
+    return
+
+
+MENU_CHOICES = (
+    (1, 'my stats', my_stats),
+    (2, 'list tweets', list_tweets),
+    (3, 'wipe timeline', wipe_timeline),
+    (9, 'quit', exit_menu)
+)
+
+
+def handle_choice(choice):
+    for menuchoice in MENU_CHOICES:
+        if int(choice) == menuchoice[0]:
+            return menuchoice[2]
+
+
+def main_menu(api):
+    apiuser = api.me()
+    print "welcome %s" % apiuser.screen_name
+    print "main menu:"
+    for menuchoice in MENU_CHOICES:
+        print "    %s: %s" % (menuchoice[0], menuchoice[1])
+    choice = raw_input('choice: ')
+
+    return handle_choice(choice)(api)
+
+
+def main():
+    if os.path.exists(ACCESS_CREDS_FILE):
+        print 'found %s! attempting to load access creds' % ACCESS_CREDS_FILE
+        try:
+            credsfile = open(ACCESS_CREDS_FILE)
+            credsdict = json.load(credsfile)
+            auth.set_access_token(credsdict['access_token'],
+                                  credsdict['access_token_secret'])
+        except ValueError:
+            print 'problem with creds file! requesting new creds'
+            get_access_creds()
+    else:
         get_access_creds()
-else:
-    get_access_creds()
 
-print "Sending a tweet..."
+    try:
+        api = tweepy.API(auth)
 
-try:
-    api = tweepy.API(auth)
-    mystatus = api.update_status('tweepy-weepy!  another tweet with python')
-except tweepy.TweepError:
-    print 'everything sucks and i can\'t tweet'
-    print 'access token: %s' % auth.access_token
-    print 'access token secret: %s' % auth.access_token_secret
-    raise
+    except tweepy.TweepError:
+        print "everything sucks!!"
+        print "access token: %s" % auth.access_token
+        print "access token secret: %s" % auth.access_token_secret
+        raise
 
+    choice = ''
 
-print "got back status:"
-pprint.pprint(mystatus)
+    while main_menu(api):
+        pass
 
+    print "byeee"
 
 
-print "AND IM SPENT"
+if __name__ == '__main__':
+    main()
