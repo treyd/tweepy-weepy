64d5e26e0ebb2216b52bfd9b43b62928c3b6f33e
Trey Duskin <trey@treyd.us>
2020-06-30 14:15:54 -0400

    py3

--- tweepy-weepy.py
+++ tweepy-weepy.py
@@ -17,18 +17,18 @@ def get_access_creds():
     try:
         redirect_url = auth.get_authorization_url()
     except tweepy.TweepError:
-        print "everything sucks and I can't get a request token..."
+        print("everything sucks and I can't get a request token...")
         raise
-    print "Ok, go here, and then tell me your PIN:"
-    print redirect_url
-    verifier = raw_input('PIN:')
+    print("Ok, go here, and then tell me your PIN:")
+    print(redirect_url)
+    verifier = input('PIN:')
     try:
         auth.get_access_token(verifier)
     except tweepy.TweepError:
-        print "everything sucks and I can't get an access token..."
+        print("everything sucks and I can't get an access token...")
         raise
-    print "My access token is: %s" % auth.access_token
-    print "I will save this token (and secret) to %s" % ACCESS_CREDS_FILE
+    print("My access token is: %s" % auth.access_token)
+    print("I will save this token (and secret) to %s" % ACCESS_CREDS_FILE)
 
     with open(ACCESS_CREDS_FILE, 'w') as credsfile:
         credsdict = {'access_token': auth.access_token,
@@ -42,7 +42,7 @@ def _get_all_tweets(api):
     tweets_fetched = api.user_timeline(count=API_REQ_COUNT)
     while len(tweets_fetched) > 0:
         tweet_list.extend(tweets_fetched)
-        print "got %d tweets" % len(tweets_fetched)
+        print("got %d tweets" % len(tweets_fetched))
         for tweet in tweets_fetched:
             if not last_id:
                 last_id = tweet.id
@@ -59,29 +59,29 @@ def exit_menu(api):
 
 
 def lookup_user(api):
-    print "who would you like to look up?"
-    confirm = raw_input('username or id: ')
+    print("who would you like to look up?")
+    confirm = input('username or id: ')
 
     try:
         tweeter = api.get_user(confirm)
-        print "User %s (%s)" % (tweeter.name, tweeter.screen_name)
-        print "Tweets: %d" % tweeter.statuses_count
-        print "Followers: %d" % tweeter.followers_count
-    except tweepy.TweepError, e:
-        print "got error from Twitter:"
-        print e.message
-    except Exception, e:
-        print "got some other error:"
-        print e.traceback
+        print("User %s (%s)" % (tweeter.name, tweeter.screen_name))
+        print("Tweets: %d" % tweeter.statuses_count)
+        print("Followers: %d" % tweeter.followers_count)
+    except tweepy.TweepError as e:
+        print("got error from Twitter:")
+        print(e.message)
+    except Exception as e:
+        print("got some other error:")
+        print(e.traceback)
 
     return True
 
 
 def my_stats(api):
     apiuser = api.me()
-    print "User %s (%s)" % (apiuser.name, apiuser.screen_name)
-    print "Tweets: %d" % apiuser.statuses_count
-    print "Followers: %d" % apiuser.followers_count
+    print("User %s (%s)" % (apiuser.name, apiuser.screen_name))
+    print("Tweets: %d" % apiuser.statuses_count)
+    print("Followers: %d" % apiuser.followers_count)
     return True
 
 
@@ -90,28 +90,28 @@ def list_tweets(api):
 
     data = ""
     for tweet in tweet_list:
-        data += unicode(tweet.created_at) + '\n'
-        data += unicode(tweet.text) + '\n'
+        data += str(tweet.created_at) + '\n'
+        data += str(tweet.text) + '\n'
         data += '-' + '\n'
 
-    print data
+    print(data)
     return True
 
 
 def wipe_timeline(api):
-    print "counting tweets..."
+    print("counting tweets...")
     tweet_list = _get_all_tweets(api)
-    print "total tweets: %d" % len(tweet_list)
-    print "This will DELETE ALL YOUR TWEETS"
-    print "you might want to go to https://twitter.com/settings/account" \
-          " and request your archive, if you care about it"
+    print("total tweets: %d" % len(tweet_list))
+    print("This will DELETE ALL YOUR TWEETS")
+    print("you might want to go to https://twitter.com/settings/account" \
+          " and request your archive, if you care about it")
 
-    confirm = raw_input('ARE YOU SURE? (type "yes" to continue): ')
+    confirm = input('ARE YOU SURE? (type "yes" to continue): ')
 
     if confirm == 'yes':
         for tweet in tweet_list:
             api.destroy_status(tweet.id)
-            print "deleted %s" % tweet.id
+            print("deleted %s" % tweet.id)
 
     return True
 
@@ -133,30 +133,30 @@ def handle_choice(choice):
 
 def main_menu(api):
     apiuser = api.me()
-    print "-----"
-    print "welcome %s" % apiuser.screen_name
-    print "main menu:"
+    print("-----")
+    print("welcome %s" % apiuser.screen_name)
+    print("main menu:")
     for menuchoice in MENU_CHOICES:
-        print "    %s: %s" % (menuchoice[0], menuchoice[1])
-    choice = raw_input('choice: ')
+        print("    %s: %s" % (menuchoice[0], menuchoice[1]))
+    choice = input('choice: ')
 
     try:
         return handle_choice(choice)(api)
     except TypeError:
-        print "invalid choice: %s" % choice
+        print("invalid choice: %s" % choice)
         return True
 
 
 def main():
     if os.path.exists(ACCESS_CREDS_FILE):
-        print 'found %s! attempting to load access creds' % ACCESS_CREDS_FILE
+        print('found %s! attempting to load access creds' % ACCESS_CREDS_FILE)
         try:
             credsfile = open(ACCESS_CREDS_FILE)
             credsdict = json.load(credsfile)
             auth.set_access_token(credsdict['access_token'],
                                   credsdict['access_token_secret'])
         except ValueError:
-            print 'problem with creds file! requesting new creds'
+            print('problem with creds file! requesting new creds')
             get_access_creds()
     else:
         get_access_creds()
@@ -165,9 +165,9 @@ def main():
         api = tweepy.API(auth)
 
     except tweepy.TweepError:
-        print "everything sucks!!"
-        print "access token: %s" % auth.access_token
-        print "access token secret: %s" % auth.access_token_secret
+        print("everything sucks!!")
+        print("access token: %s" % auth.access_token)
+        print("access token secret: %s" % auth.access_token_secret)
         raise
 
     choice = ''
@@ -175,7 +175,7 @@ def main():
     while main_menu(api):
         pass
 
-    print "byeee"
+    print("byeee")
 
 
 if __name__ == '__main__':
